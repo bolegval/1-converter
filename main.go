8804506608da809fd7335c5aecc30c15eef01620
bondarenko.o <bondarenko_oleg@list.ru>
2025-10-08 18:57:12 +0300

    ДЗ 3.11

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	const USD_TO_EUR float32 = 0.94
 	const USD_TO_RUB float32 = 92.0
 	const EUR_TO_RUB float32 = 1 / USD_TO_EUR * USD_TO_RUB
-
-	fmt.Println(EUR_TO_RUB)
 }
