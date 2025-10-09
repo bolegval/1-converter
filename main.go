9fd75e0b0803a8e333ba5c8fc3eb42bdb807a04d
bondarenko.o <bondarenko_oleg@list.ru>
2025-10-09 19:07:41 +0300

    Подготовка функций

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USD_TO_EUR float32 = 0.94
 	const USD_TO_RUB float32 = 92.0
 	const EUR_TO_RUB float32 = 1 / USD_TO_EUR * USD_TO_RUB
+}
+
+func getUserInput() string {
+	var currency string
+	fmt.Print("Введите значение: ")
+	fmt.Scan(&currency)
+
+	return currency
+}
+
+func calculate(value float64, currencyFrom string, currencyTo string) (result float64) {
+	return result
 }
